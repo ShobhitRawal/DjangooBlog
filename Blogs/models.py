3701858d156470d87a0f0d13bd10aba1cce266c1
Shobhit <shobhitkumarrawal92@gmail.com>
2018-09-03 19:20:16 +0530

    idea file deleted

--- Blogs/models.py
+++ Blogs/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class create_profile(models.Model):
     user_name = models.CharField(max_length=120)
     email= models.EmailField(max_length=120,unique=True,primary_key=True)
--- Blogs/views.py
+++ Blogs/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import create_profile
 # Create your views here.
 def home_view(request):
     return render(request,'Blogs/home.html')
 
-def create_profile(request):
+def create_User_profile(request):
     return render(request,'Blogs/index.html')
+    name = request.POST.get('user')
+    print(name)
+    useremail = request.POST.get('email')
+    print(useremail)
+    passw= request.POST.get('password')
+    obj = create_profile()
+    obj.user_name = name
+    obj.email = useremail
+    obj.password = passw
+    obj.save()
+    
+    
